234ac8ea2f407ccea0be2a754c23b67f6bb9b5db
jhart2 <jharttech@hotmail.com>
2020-07-16 13:04:16 -0500

    Updated directory and file structure for ease of use.

--- script.sh
+++ script.sh
@@ -7,6 +7,8 @@
 # - Initial roll out of script
 # 20200716 Hartgraves
 # - Updated awk call to adjust for import requirements
+# 20200716 Hartgraves
+# - Updated directory and file structures
 ###############################################################
 # Note this script requires files from GAM that are not provided
 # in the repository
@@ -23,13 +25,15 @@ while true; do
 			if [ "$NumCarts" -gt 1 ];
 			then
 				counter=1
+				mkdir carts
+				cd carts
 				mkdir $Building
 				echo "Now going to get cart info and write cart files"
 				sleep 3
 				while [ "$counter" -le "$NumCarts" ];
 				do
-					cp cart_template.csv "$Building"/"$Building"CB"$counter".csv # This line requires files from GAM
-					cat needed_file/full.csv | grep -w ""$Building"CB"$counter"" | awk -F, '{print "echo -n "$1",;echo -n $(date -d @$(("$54" / 1000)) +%Y%m%d);echo -n ,"$1467","$468","$470",initial import,"$472","$470",chromebook,"; if (length($1467)>13) {print "echo "substr($1467,length($1467)-13,length($1467))}else{print "echo "$1467}}' | sh >> "$Building"/"$Building"CB"$counter".csv # This line requires files from GAM
+					cp ../cart_template.csv "$Building"/"$Building"CB"$counter".csv # This line requires files from GAM
+					cat ../needed_file/full.csv | grep -w ""$Building"CB"$counter"" | awk -F, '{print "echo -n "$1",;echo -n $(date -d @$(("$54" / 1000)) +%Y%m%d);echo -n ,"$1467","$468","$470",initial import,"$472","$470",chromebook,"; if (length($1467)>13) {print "echo "substr($1467,length($1467)-13,length($1467))}else{print "echo "$1467}}' | sh >> "$Building"/"$Building"CB"$counter".csv # This line requires files from GAM
 					counter=$((counter+1))
 				done
 				exit
@@ -41,8 +45,10 @@ while true; do
 				then
 					echo "Getting information for cart "$CartName" and writing to file 'single/"$CartName".csv'."
 					sleep 3
+					mkdir carts
+					cd carts
 					mkdir single
-					cp cart_template.csv single/"$CartName".csv # This line requires files from GAM
+					cp ../cart_template.csv single/"$CartName".csv # This line requires files from GAM
 					cat needed_file/full.csv | grep -w ""$CartName"" | awk -F, '{print "echo -n "$1",;echo -n $(date -d @$(("$54" / 1000)) +%Y%m%d);echo -n ,"$1467","$468","$470",initial import,"$472","$470",chromebook,"; if (length($1467)>13) {print "echo "substr($1467,length($1467)-13,length($1467))}else{print "echo "$1467}}' | sh >> single/"$CartName".csv # This line requires files from GAM
 					break
 				fi
