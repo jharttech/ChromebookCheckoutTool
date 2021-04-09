3419bf2d2906438e4def08ef17088a7ad80f81f9
Jason Hartgraves <jharttech@hotmail.com>
2021-04-09 11:21:09 -0500

    Added getStudentInfo function for populating students by building.

--- scripts/user_script.py
+++ scripts/user_script.py
@@ -4,6 +4,17 @@ import subprocess
 
 def main():
     getWantedData()
+    building = getBuilding()
+    if building == 'ALL':
+        file = '../needed_file/studentFull.csv'
+        dest = '../students/studentFull.csv'
+        subprocess.call(['mv',file,dest])
+        print("All student data has been compiled into ..ChromebookCheckoutTool/students/studentFull.csv")
+        exit()
+    else:
+        getStudentsInfo(building)
+
+
 
 def getWantedData():
     headerList = ['primaryEmail', 'name.givenName', 'name.familyName', 'orgUnitPath']
@@ -40,15 +51,72 @@ def getWantedData():
                             notes, username]
                 lines.append(tempRow)
         if tempRow != []:
-            with open('../needed_file/fullStudent.csv', mode='w') as student_file:
+            with open('../needed_file/studentFull.csv', mode='w') as student_file:
                 for i in range(0,len(lines)):
                     studentFull = csv.writer(student_file, delimiter=',')
                     studentFull.writerow(lines[i])
 
-def getStudentOU():
-    studentBuilding = getBuilding()
+def getStudentsInfo(building):
+    with open('../needed_file/studentFull.csv', mode='r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        lines = []
+        nCol = len(next(csv_reader))
+        csv_file.seek(0)
+        for row in csv_reader:
+            if line_count == 0:
+                for x in range(0,nCol):
+                    columnName = str(row[x])
+                    if columnName == 'Location':
+                        num = x
+                lines.append(row)
+                line_count += 1
+            else:
+                tempBuilding = row[num].split('/')
+                tempBuilding = tempBuilding[len(tempBuilding) - 1].upper()
+                if tempBuilding == building:
+                    lines.append(row)
+    with open('../students/' + building + '.csv', mode='w') as student_file:
+        for i in range(0,len(lines)):
+            studentFile = csv.writer(student_file, delimiter=',')
+            studentFile.writerow(lines[i])
 
 def getBuilding():
-    return
+    valid = False
+    while not valid:
+        buildingList = getBuildingNames()
+        buildingList.append('ALL')
+        building = input("Please enter the building wanted (" + str(', '.join(buildingList)) + "): ")
+        building = building.upper()
+        if building in buildingList:
+            valid = True
+            print("You chose", building)
+            correct = input("Is that the correct building? (y/n) ")
+            if correct.lower() != 'y':
+                valid = False
+            else:
+                return building
+
+def getBuildingNames():
+    buildingList = []
+    tempBuilding = []
+    num = None
+    with open('../needed_file/studentFull.csv', mode='r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        nCol = len(next(csv_reader))
+        csv_file.seek(0)
+        for row in csv_reader:
+            if line_count == 0:
+                for x in range(0,nCol):
+                    columnName = str(row[x])
+                    if columnName == 'Location':
+                        num = x
+                        line_count += 1
+            else:
+                tempBuilding = row[num].split('/')
+                if tempBuilding[len(tempBuilding) - 1] not in buildingList:
+                    buildingList.append(tempBuilding[len(tempBuilding) - 1])
+    return buildingList
 
 main()
