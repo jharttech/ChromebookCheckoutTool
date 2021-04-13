6ca365d3e38ecc324fe0805c251f3a9203a5759a
Jason Hartgraves <jharttech@hotmail.com>
2021-04-13 09:58:42 -0500

    Added logic to check for duplicate entry and log the error.

--- scripts/hotspot_script.py
+++ scripts/hotspot_script.py
@@ -1,6 +1,7 @@
 import csv
 import os
 import subprocess
+import datetime
 
 def main():
     info = getHotspotInfo()
@@ -47,38 +48,76 @@ def getHotspotInfo():
 
 def createHotspotEntry(info):
     counter = info.get('count')
+    divider = ['###########']
     hotspotDict = {}
     tempRow = []
+    lines = []
+    dictLineToDelete = []
     line_count = 0
+    keyNum = 0
+    error = False
     valid = False
     headerList = ['Category', 'Model Name', 'Location', 'Manufacturer',
                     'Purchase Date', 'Purchase Cost', 'Order Number',
                     'Asset Tag', 'Serial Number', 'Sim Card Number']
-    file = str(info.get('isoDate') + "-PO" + info.get('poNumber') + "-hotspots.csv")
-    errorFile = 'errorLog.txt'
-    masterFile = '../needed_file/hotspot_master_list.csv'
+    file = ('../hotspots/' + str(info.get('isoDate') + "-PO" + info.get('poNumber') + "-hotspots.csv"))
+    errorFile = '../hotspots/errorLog.csv'
+    masterFile = '../hotspots/hotspot_master_list.csv'
     subprocess.call(['touch',masterFile])
     print("Now going to ask for information about each hotspot being added.")
-    for i in range(0,counter):
+    for n in range(0,counter):
         valid = False
+        error = False
         while valid == False:
-            serialNumber = input("Please enter the Serail Number (no spaces) for hotspot " + str(i + 1) + ": ")
-            simCard = input("Please enter the Sim Card Number (no spaces) for hotspot " + str(i + 1) + ": ")
-            print("You have entered hotspot " + str(i + 1) + "'s Serial Number as: " + str(serialNumber))
+            serialNumber = input("Please enter the Serial Number (no spaces) for hotspot " + str(n + 1) + ": ")
+            simCard = input("Please enter the Sim Card Number (no spaces) for hotspot " + str(n + 1) + ": ")
+            print("You have entered hotspot " + str(n + 1) + "'s Serial Number as: " + str(serialNumber))
             print("and Sim Card Number as: " + simCard)
             response = input("Is that information correct? (y/n): ").lower()
             if response == 'y':
                 valid = True
-                hotspotDict.update({i + 1 : {'Serial Number' : serialNumber,'Sim Card Number' : simCard}})
-    with open(masterFile, mode='r') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for row in csv_reader:
+                subprocess.call(['touch',file])
+                with open(masterFile, mode='r') as csv_file:
+                    csv_reader = csv.reader(csv_file, delimiter=',')
+                    for row in csv_reader:
+                        if serialNumber in row:
+                            error = True
+                            print("Duplicate detected with Serial Number: " + serialNumber)
+                            print("This hotspot record will not be added to csv file. This is recorded in ...ChromebookCheckoutTool/hotspots/errorLog.csv file.")
+                            subprocess.call(['touch',errorFile])
+                            timestamp = datetime.datetime.now()
+                            tempLine = (timestamp, 'DUPLICATE ERROR', 'hotspot', info.get('brand'), 'MG Schools', info.get('manufacturer'),
+                                        info.get('isoDate'), info.get('cost'), info.get('poNumber'),
+                                        serialNumber, serialNumber, simCard)
+                            with open(errorFile, mode='a') as error_file:
+                                errors = csv.writer(error_file, delimiter=',')
+                                errors.writerow(tempLine)
+                                errors.writerow(divider)
+            if error == False:
+                hotspotDict.update({keyNum + 1 : {'Serial Number' : serialNumber,'Sim Card Number' : simCard}})
+                keyNum += 1
+                if counter == 1:
+                    print("Nothing to be done... quitting program now! Yes, I am a quitter.")
+                    exit()
             line_count += 1
         if line_count == 0:
-            with open('../needed_file/' + masterFile, mode='w') as hotspot_file:
-                hotspotFile = csv.writer(hotspot_file, delimiter=',')
-                hotspotFile.writerow(headerList)
-                line_count += 1
+            lines.append(headerList)
+            line_count += 1
+    for x in range(0,len(hotspotDict)):
+        tempRow = ['hotspot', info.get('brand'), 'MG Schools', info.get('manufacturer'),
+                    info.get('isoDate'), info.get('cost'), info.get('poNumber'),
+                    hotspotDict[x + 1]['Serial Number'], hotspotDict[x + 1]['Serial Number'],
+                    hotspotDict[x + 1]['Sim Card Number']]
+        lines.append(tempRow)
+    if lines != []:
+        with open(masterFile, mode='a') as hotspot_file:
+            for i in range(0,len(lines)):
+                hotspotMaster = csv.writer(hotspot_file, delimiter=',')
+                hotspotMaster.writerow(lines[i])
+        with open(file, mode='w') as hotspotCsv:
+            for z in range(0,len(lines)):
+                hotspotCSV = csv.writer(hotspotCsv, delimiter=',')
+                hotspotCSV.writerow(lines[z])
     return hotspotDict
 
 main()
