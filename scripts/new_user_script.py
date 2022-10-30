61965dfabd27af1fafbfcb8163fc78fed3fd2702
Jason Hartgraves <jharttech@hotmail.com>
2022-10-30 11:40:12 -0500

    writing the Stage_csv Class

--- scripts/new_user_script.py
+++ scripts/new_user_script.py
@@ -26,34 +26,84 @@ class Account_type:
                 return account
 
 
-class Compose_csv:
+class Stage_csv:
     def __init__(self,account_type):
-        self.header = [
+        self.g_headers = [
             'primaryEmail',
             'name.givenName',
             'name.familyName',
             'orgUnitPath'
         ]
         self.header_to_num = {}
-        lines = []
-        temp_row = []
-        num = None
+        self.lines = []
+        self.temp_row = []
+        self.num = None
 
-        compose(account_type)
+        stage(self.g_headers,self.header_to_num,account_type
+        ,self.lines)
 
-    def compose(self.account_type):
+    def stage(self,g_headers,header_to_num,account_type,lines):
         if account_type == "staff":
-            this.filename = "full_staff.csv"
+            this.i_filename = "full_staff.csv"
+            this.o_filename = "fullStaff.csv"
         elif account_type == "student":
-            this.filename = "full_student.csv"
+            this.i_filename = "full_student.csv"
+            this.o_filename = "fullStudent.csv"
         else:
             raise ValueError
 
-        with open(f"needed_file/{this.filename}") as csv_file:
+        with open(f"needed_file/{this.i_filename}") as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             n_col = len(next(csv_reader))
             csv_file.seek(0)
-            line_count = 0 
+            line_count = 0
+            header_row = ['Email','First Name','Last Name','Location','Notes','Username']
+            notes = 'EMPLOYEE'
+
+            lines.append(header_row)
+
+            for row in csv_reader:
+                while line_count == 0:
+                    for x in range(0,n_col):
+                        col_name = str(row[x])
+                        if col_name in header:
+                            header_to_num.update({col_name:x})
+                    line_count += 1
+                
+                try:
+                    username = row[header_to_num.get('primaryEmail',"Error getting header number for primaryEmail")].split('@')
+                    username = username[0]
+                except:
+                    sys.exit(f"Error with primaryEmail field, please check the 'needed_file/{this.filename}'")
+
+                try:
+                    temp_row = [
+                        row[header_to_num.get('primaryEmail',"Error getting header number for primaryEmail")],
+                        row[header_to_num.get('name.givenName',"Error getting header number for givenName")],
+                        row[header_to_num.get('name.familyName',"Error getting header number for familyName")],
+                        row[header_to_num.get('orgUnitPath',"Error getting header number for orgUnitPath")],
+                        notes,
+                        username
+                    ]
+                    lines.append(temp_row)
+                except:
+                    sys.exit(f"Error getting needed fields for csv row")
+
+            if temp_row != []:
+                with open(f'needed_file/{this.o_filename}.csv', mode = 'w') as csv_file:
+                    for i in range(0,len(lines)):
+                        full = csv.writer(csv_file,delimiter=',')
+                        full.writerow(lines[i])
+            else:
+                sys.exit(f"Error: no {account_type} data to add.  Now going to exit...")
+
+                    
+
+
+
+
+
+
              
 
 
@@ -64,7 +114,7 @@ def main():
     if account_type == "exit":
         sys.exit("You have chosen to exit.")
     else:
-        Compose_csv(account_type)
+        staged = Stage_csv(account_type)
 
 
 if __name__ == "__main__":
