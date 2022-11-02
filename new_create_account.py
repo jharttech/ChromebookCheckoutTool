c4109def1a649928222675ab2a249e9ebdfb413b
Jason Hartgraves <jharttech@hotmail.com>
2022-11-02 15:37:05 -0500

    setting up CampusOU class.

--- new_create_account.py
+++ new_create_account.py
@@ -6,8 +6,21 @@ from scripts import new_user_script
 class Setup():
     def __init__(self,account_type):
         self.account_type = account_type
-        n_file = f"{account_type}.txt"
-        subprocess.Popen(["touch",])
+        n_file = f"{account_type}/{account_type}.txt"
+        subprocess.Popen(["touch",n_file], stdout=subprocess.PIPE)
+
+
+class Campus_OUs:
+    def __init__(self):
+        all_ou = subprocess.Popen(["gam","print","orgs"], stdout=subprocess.DEVNULL)
+        print(all_ou)
+
+
+class Staff_OU:
+    def __init__(self):
+        self.org_unit_dict = {
+            
+        }
 
 
 def main():
@@ -15,6 +28,9 @@ def main():
     print("Welcome to the MG Create Account Tool\n")
     account_type = new_user_script.Account_type.get()
     Setup(account_type)
+    Campus_OUs()
+    if str(account_type) == "staff":
+        Staff_OU
 
 
 if __name__=="__main__":
