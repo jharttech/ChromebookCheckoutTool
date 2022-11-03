d4ae3f4d4def5bbfb93c9b84a99be3057499f84e
Jason Hartgraves <jharttech@hotmail.com>
2022-11-03 16:00:45 -0500

    working on the dynamic ou dict.

--- new_create_account.py
+++ new_create_account.py
@@ -12,30 +12,56 @@ class Setup():
 
 
 class Campus_OUs:
-    def __init__(self):
+    def __init__(self, account_type):
         ...
 
 
-    def ou_dict(self):
+
+    def ou_dict(self, account_type):
     #ADD on campus and logic to build the dictionary for staff or student
-        #all_ou = subprocess.Popen(["gam","print","orgs"], stdout=subprocess.DEVNULL)
-        #print(all_ou)
-        # GETOU = "gam print orgs"
+        self.account_type = account_type
+        
+        if str(self.account_type) == "staff":
+            self.ou_type = "Employees"
+        else:
+            self.ou_type = "Students"
+        
+        self.org_unit_dict = {}
+        self.all_ou = subprocess.Popen(["gam","print","orgs"], stdout=subprocess.PIPE)
+        self.result = all_ou.stdout.read().decode()
+        self.all_ou.wait()
+        with open(f"needed_file/result.csv", mode='w') as self.temp_file:
+            self.temp_file.write(result)
+
+        with open(f"needed_file/result", mode='r') as self.csv_file_read:
+            self.csv_reader = csv.reader(self.csv_file_read, delimiter=',')
+            self.n_col = len(next(self.csv_reader))
+            self.line_count = 0
+            for row in self.csv_reader:
+                if self.line_count = 0:
+                    for x in range(0, self.n_col):
+                        self.col_name = str(row[x])
+                        if str(self.col_name) == "orgUnitPath":
+                            self.num = x
+                            self.line_count += 1
+                else:
+                    if self.ou_type in str(row[x]):
+                        self.org_unit_dict.update({f"{x}":self.str(row[x])})    # GETOU = "gam print orgs"
         # listOU = list(subprocess.call(GETOU, shell=True))
         # for x in range(0,len(listOU)):
         #     orgUnitDict.update({x + 1 : listOU[x]})
         # orgUnitDict.update({int(len(listOU)) : "Exit"})
         #Above code needs implimented once on campus
-        self.org_unit_dict = {
-            "1": "Employees",
-            "2": "Admins",
-            "3": "Discovery",
-            "4": "LTSubs",
-            "5": "SecurityStrong",
-            "6": "Technology",
-            "7": "BoardMembers",
-            "8": "Exit"
-            }
+        #self.org_unit_dict = {
+            #"1": "Employees",
+            #"2": "Admins",
+            #"3": "Discovery",
+            #"4": "LTSubs",
+            #"5": "SecurityStrong",
+            #"6": "Technology",
+            #"7": "BoardMembers",
+            #"8": "Exit"
+            #}
 
         return self.org_unit_dict
 
