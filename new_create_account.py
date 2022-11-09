83e972aeb55b584a9747101615fe952541644ec3
Jason Hartgraves <jharttech@hotmail.com>
2022-11-09 14:53:46 -0600

    removed debug comments, added restart feature.

--- new_create_account.py
+++ new_create_account.py
@@ -17,7 +17,6 @@ class Campus_OUs:
         ...
 
     def ou_dict(self, account_type):
-        # ADD on campus and logic to build the dictionary for staff or student
         self.account_type = account_type
 
         if str(self.account_type) == "staff":
@@ -26,11 +25,6 @@ class Campus_OUs:
             self.ou_type = "Students"
 
         self.org_unit_dict = {}
-        # self.all_ou = subprocess.Popen(["gam","print","orgs"], stdout=subprocess.PIPE)
-        # self.result = all_ou.stdout.read().decode()
-        # self.all_ou.wait()
-        # with open(f"needed_file/result.csv", mode='w') as self.temp_file:
-        # self.temp_file.write(result)
 
         with open(f"needed_file/result.csv", mode="r") as self.csv_file_read:
             self.read_file = csv.reader(self.csv_file_read, delimiter=",")
@@ -54,21 +48,6 @@ class Campus_OUs:
                         self.org_unit_dict.update(
                             {str(self.dict_key_count): row[self.num]}
                         )
-        # listOU = list(subprocess.call(GETOU, shell=True))
-        # for x in range(0,len(listOU)):
-        #     orgUnitDict.update({x + 1 : listOU[x]})
-        # orgUnitDict.update({int(len(listOU)) : "Exit"})
-        # Above code needs implimented once on campus
-        # self.org_unit_dict = {
-        # "1": "Employees",
-        # "2": "Admins",
-        # "3": "Discovery",
-        # "4": "LTSubs",
-        # "5": "SecurityStrong",
-        # "6": "Technology",
-        # "7": "BoardMembers",
-        # "8": "Exit"
-        # }
 
         return self.org_unit_dict
 
@@ -82,7 +61,6 @@ class Assign_OU:
 
     @classmethod
     def get(cls, ou_dict):
-        # DEBUG print(ou_dict)
         while True:
             choice = input("Please select the desired Org Unit: ")
             if str(choice) not in ou_dict:
@@ -95,11 +73,6 @@ class Assign_OU:
 class Campus_groups:
     def __init__(self):
         ...
-
-    # Create logic to poll GAM for groups, drop them in a list,
-    # then ask the user to input what groups they want the user to
-    # be a member of
-    # gam print groups
 
     def groups_dict(self):
         self.group_dict = {}
@@ -121,16 +94,11 @@ class Campus_groups:
                     self.line_count += 1
                     self.dict_key_count += 1
                     self.group_dict.update({str(self.dict_key_count): row[self.num]})
-        # self.group_dict = {
-        #   "1":"staff_print",
-        #  "2":"all_staff",
-        # "3":"classroom"
-        # }
+
         return self.group_dict
 
 
 class Assign_groups:
-    # Finish this class, may use getter setter
     def __init__(self, assigned_groups, account_type):
         self.assigned_groups = assigned_groups
         self.account_type = account_type
@@ -170,11 +138,6 @@ class Add_to_group:
                         self.run_gam.wait()
                     except FileNotFoundError as e:
                         raise (e)
-
-
-def dict_print(data):
-    print("\n")
-    [print(key, ":", value) for key, value in data.items()]
 
 
 class Create_Account:
@@ -245,6 +208,11 @@ class Create_Account:
                 raise (e)
 
 
+def dict_print(data):
+    print("\n")
+    [print(key, ":", value) for key, value in data.items()]
+
+
 def main():
     subprocess.Popen(["clear"], stdout=subprocess.PIPE)
     print("Welcome to the MG Create Account Tool\n")
@@ -263,7 +231,19 @@ def main():
         # Add_to_group(groups,account_type)
     else:
         Create_Account(account_type, OU)
-    print(OU)
+
+    while True:
+        restart = input(
+            f"Account creations and group additions sucessfull. Would you like to create more accounts? y/n "
+        ).lower()
+        if not re.search(r"^(y|n)$", restart):
+            print(f"Invalid response please enter 'y' or 'n'")
+        else:
+            break
+    if str(restart) == "y":
+        main()
+    else:
+        sys.exit("Thank you!  -Jhart")
 
 
 if __name__ == "__main__":
