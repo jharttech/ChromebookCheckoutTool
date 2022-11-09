052a504f929dc7119c2285a32b90a1a4859f2152
Jason Hartgraves <jharttech@hotmail.com>
2022-11-09 14:45:30 -0600

    Finished up rough draft of the new create_account tool.

--- new_create_account.py
+++ new_create_account.py
@@ -130,17 +130,17 @@ class Campus_groups:
     
 class Assign_groups:
     #Finish this class, may use getter setter 
-    def __init__(self, groups):
-        self.group_list(groups)
-
-    
-    def group_list(self,groups):
-        self.groups = groups
-        return self.groups
+    def __init__(self,assigned_groups,account_type):
+        self.assigned_groups = assigned_groups
+        self.account_type = account_type
+        self.groups = assigned_groups
+        print(self.groups)
+        Add_to_group(self.groups,self.account_type)
+        
         
         
     @classmethod
-    def get(cls,campus_groups):
+    def get(cls,campus_groups,account_type):
         assigned_groups = []
         
         group_wanted = input(f"""\nPlease enter the numbers of the groups
@@ -149,12 +149,23 @@ the user will need be a member of: (Comma seperated: ex. 1,2,3)\n""")
         for i in range(0,len(group_wanted)):
             assigned_groups.append(campus_groups.get(group_wanted[i]))
         
-        return cls(assigned_groups)
+        return cls(assigned_groups,account_type)
     
     
 class Add_to_group:
-    def __init__(self,groups,staff_user):
-        ...
+    def __init__(self,groups,account_type):
+        self.groups = groups
+        with open(f"{account_type}/{account_type}.txt") as self.file:
+            self.reader = csv.reader(self.file, delimiter=':')
+            for row in self.reader:
+                for x in range(0,len(self.groups)):
+                    try:
+                        self.command = (f"gam update group {self.groups[x]} add user {row[0]}")
+                        self.run_gam = subprocess.Popen([self.command], stdout=subprocess.PIPE)
+                        self.run_gam.wait()   
+                    except FileNotFoundError as e:
+                        raise(e)
+
             
 def dict_print(data):
     print("\n")
@@ -162,7 +173,7 @@ def dict_print(data):
 
 
 class Create_Account:
-    def __init__(self,account_type,wanted_ou,groups):
+    def __init__(self,account_type,wanted_ou):
         self.account_type = account_type
         self.sed_params = (f"s,$,:{wanted_ou},")
         self.temp_file = f"{account_type}/temp_{account_type}.txt"
@@ -179,18 +190,18 @@ class Create_Account:
             self.inject_org = subprocess.Popen(["sed","-e",self.sed_params,self.temp_file], stdout=self.file)
             self.inject_org.wait()
             self.inject_org.communicate()
-        
             
         input("stop")
 
-        self.gam(self.account_type,wanted_ou,groups,self.awk_file)
+        self.gam(self.account_type,wanted_ou,self.awk_file)
 
     def copy_file(self,temp_file,awk_file):
         cp = subprocess.Popen(["cp",temp_file,awk_file], stdout=subprocess.PIPE)
         cp.wait()
 
-    def gam(self,account_type,wanted_ou,groups,awk_file):
+    def gam(self,account_type,wanted_ou,awk_file):
         self.account_type = account_type
+        self.awk_file = awk_file
 
         if str(self.account_type) == "student":
             self.awk_line = '{print "gam create user "$1" firstname "$2" lastname "$3" password "$4" gal off org "$5" && sleep 2"}'
@@ -210,7 +221,7 @@ class Create_Account:
                 break
 
         if yn == "n":
-            self.__init__(account_type,wanted_ou,groups)
+            self.__init__(account_type,wanted_ou)
         else:
             try:
                 self.holder = subprocess.Popen([self.gam_command], stdout=subprocess.PIPE)
@@ -218,12 +229,9 @@ class Create_Account:
                 print(f"{self.run.stdout.read().decode()}")
                 self.run.communicate()
                 self.run.wait()
-            except FileNotFoundError as e: #DEBUG
-                print(f"Error: {e.args[1]} (")
-                #print(sys.exc_info()[0])
-                #sys.exit("There was an error trying to create the accounts. Exiting now!")
-            finally:
-                print("finally")
+            except FileNotFoundError as e:
+                raise(e)
+                    
 
 def main():
     subprocess.Popen(["clear"], stdout=subprocess.PIPE)
@@ -236,14 +244,13 @@ def main():
     OU = Assign_OU(None).get(campus_OUs)
     
     if str(account_type) == "staff":
+        Create_Account(account_type,OU)
         campus_groups = Campus_groups().groups_dict()
         dict_print(campus_groups)
-        groups = Assign_groups(None).get(campus_groups)
-        Create_Account(account_type,OU,groups)
-        Add_to_group(groups,staff_user)
+        groups = Assign_groups.get(campus_groups,account_type)
+        #Add_to_group(groups,account_type)
     else:
-        Create_Account(account_type,OU,None)
-    
+       Create_Account(account_type,OU)    
     print(OU)
 
 
