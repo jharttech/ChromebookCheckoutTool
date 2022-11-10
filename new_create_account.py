836c37356a3c725a4d0e3fb37fdcdfda380d6548
Jason Hartgraves <jharttech@hotmail.com>
2022-11-09 19:56:48 -0600

    Adding comment, also consolidated code by doing away with different logic flows if staff or student.

--- new_create_account.py
+++ new_create_account.py
@@ -5,17 +5,26 @@ import csv
 from scripts import new_user_script
 
 
+# The Setup Class creates the needed Directory and then creates an empyt file that will be needed
 class Setup:
     def __init__(self, account_type):
+        # Create a directory with the account type the user chose
+        subprocess.Popen(["mkdir",str(account_type)], stdout=subprocess.DEVNULL)
         self.account_type = account_type
+        # Assign the new file and path to a variable
         n_file = f"{account_type}/{account_type}.txt"
+        # Create the empty file 
         subprocess.Popen(["touch", n_file], stdout=subprocess.PIPE)
 
 
+# The Campus_OUs class gathers the campus OU's and puts them into a dictionary with numeric keys
 class Campus_OUs:
+    # We do not need the init method for this class so we move on
     def __init__(self):
         ...
 
+    # The ou_dict method takes only the relevant Org Units for the desired account type and
+    # Assigns them to a dictionary with numeric keys
     def ou_dict(self, account_type):
         self.account_type = account_type
 
@@ -26,25 +35,36 @@ class Campus_OUs:
 
         self.org_unit_dict = {}
 
+        # This will need changed when in production, rather than just read from a file
+        # This will poll for the Org Units from GAM, then read the subprocess stdout
+        # And assign the results to the results file first
         with open(f"needed_file/result.csv", mode="r") as self.csv_file_read:
             self.read_file = csv.reader(self.csv_file_read, delimiter=",")
+            # Read the number of columns
             self.n_col = len(next(self.read_file))
+            # Return the reader back to start of file
             self.csv_file_read.seek(0)
+            # Keep track of line
             self.line_count = 0
+            # Keep track of numeric value of the key
             self.dict_key_count = 0
             for row in self.read_file:
-                print(f"row is {row}")
                 if self.line_count == 0:
+                    # Look through columns
                     for x in range(0, self.n_col):
+                        # Set the column name to the header value of each column
                         self.col_name = str(row[x])
+                        # Find the number of the column that has the header name of orgUnitPath
                         if str(self.col_name) == "orgUnitPath":
                             self.num = x
+                            # Increase line count
                             self.line_count += 1
                 else:
                     self.line_count += 1
-                    print(str(self.ou_type))
                     if str(self.ou_type) in row[self.num]:
+                        # Increase the key count so the numeric values start at 1 and not 0
                         self.dict_key_count += 1
+                        # Add the numberic key and the value to the dictionary of Org Units
                         self.org_unit_dict.update(
                             {str(self.dict_key_count): row[self.num]}
                         )
@@ -52,6 +72,7 @@ class Campus_OUs:
         return self.org_unit_dict
 
 
+# The Assign_OU class askes the user which Org Unit they want to assign the new user(s) to.
 class Assign_OU:
     def __init__(self, ou):
         self.ou = ou
@@ -60,56 +81,78 @@ class Assign_OU:
         return self.ou
 
     @classmethod
+    # Create the get class method to take the users input
     def get(cls, ou_dict):
+        # Create a loop to help with invalid inputs
         while True:
             choice = input("Please select the desired Org Unit: ")
+            # Check to see if the input matches any of the keys
             if str(choice) not in ou_dict:
+                # If user input was not in the numeric keys, prompt them to enter a number
+                # Between 1 and the length of the dictionary
                 print(f"Invalid entry, please try again! (Enter 1-{len(ou_dict)})")
             else:
                 ou = ou_dict.get(choice)
                 return cls(ou)
 
 
+# The Campus_groups class poll GAM for the campus groups, then assignes them as values to a numeric key
 class Campus_groups:
     def __init__(self):
         ...
 
+    # The groups_dict method in production will poll GAM for the campus groups
+    # Put the stdout into a file, then read the file to create the desired dictionary
     def groups_dict(self):
         self.group_dict = {}
+        # Open the group_data file for reading
         with open(f"needed_file/group_data.csv", mode="r") as self.csv_file_read:
             self.read_file = csv.reader(self.csv_file_read, delimiter=",")
+            # Read the number of columns
             self.n_col = len(next(self.read_file))
+            # Move the reader back to the start of the file
             self.csv_file_read.seek(0)
+            # Keep track of the lines
             self.line_count = 0
+            # Keep count for the numeric dictionary keys
             self.dict_key_count = 0
             for row in self.read_file:
-                print(f"row is {row}")
                 if self.line_count == 0:
+                    # Loop through the header columns
                     for x in range(0, self.n_col):
+                        # Assing col_name to each header column value
                         self.col_name = str(row[x])
+                        # Look for the number of the column thats header value is Email
                         if str(self.col_name) == "Email":
                             self.num = x
                             self.line_count += 1
                 else:
                     self.line_count += 1
                     self.dict_key_count += 1
+                    # Write the keys and values to the dictionary
                     self.group_dict.update({str(self.dict_key_count): row[self.num]})
+        
+        # Append the NO GROUPS value to the dictionary        
+        self.group_dict.update({str(self.dict_key_count + 1): "NO GROUPS"})
 
         return self.group_dict
 
-
+# The Assign_groups class askes the user what groups they user needs to be a part of if
 class Assign_groups:
     def __init__(self, assigned_groups, account_type):
         self.assigned_groups = assigned_groups
         self.account_type = account_type
         self.groups = assigned_groups
-        print(self.groups)
+        # start the Add_to_group Class
         Add_to_group(self.groups, self.account_type)
 
     @classmethod
+    # Create the get method so the user can input which groups are needed for the user
     def get(cls, campus_groups, account_type):
         assigned_groups = []
 
+        # Take user input of the keys for which groups are needed.
+        ## NOTE strip out whitespace incase user inputs spaces and use regex to check input
         group_wanted = input(
             f"""\nPlease enter the numbers of the groups
 the user will need be a member of: (Comma seperated: ex. 1,2,3)\n"""
@@ -228,9 +271,6 @@ def main():
         campus_groups = Campus_groups().groups_dict()
         dict_print(campus_groups)
         groups = Assign_groups.get(campus_groups, account_type)
-        # Add_to_group(groups,account_type)
-    else:
-        Create_Account(account_type, OU)
 
     while True:
         restart = input(
